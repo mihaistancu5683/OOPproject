7ee6de47238d6dde2dc585f6a1ebc25e390cfcfd
Mihai <mihaistancu@mail.com>
2018-04-11 19:56:11 +0300

    Corrects operator implementation

--- Fraction/Fraction/Fraction.h
+++ Fraction/Fraction/Fraction.h
@@ -7,6 +7,13 @@ public:
 	Fraction(T numerator, T denominator = (T)1); //two arguments constructor
 	~Fraction(); //destructor
 
+	bool operator == (Fraction<T>);
+
+	Fraction<T> operator + (Fraction<T>);
+	Fraction<T> operator - (Fraction<T>);
+	Fraction<T> operator * (Fraction<T>);
+	Fraction<T> operator / (Fraction<T>);
+
 	T _numerator;
 	T _denominator;
 
@@ -30,58 +37,58 @@ Fraction<T>::~Fraction() //destructor
 }
 
 template <class T>
-bool operator == (Fraction<T> & lhs, Fraction<T> & rhs) // == operator
+bool Fraction<T>::operator == (Fraction<T> fraction) // == operator
 {
-	if (abs(lhs._numerator * rhs._denominator -
-		lhs._denominator * rhs._numerator) < 0.000001)
+	if (abs(this->_numerator * fraction._denominator -
+		    this->_denominator * fraction._numerator) < 0.000001)
 		return true;
 	else
 		return false;
 }
 
 template <class T>
-Fraction<T> operator + (Fraction<T> & lhs, Fraction<T> & rhs) // + operator
+Fraction<T> Fraction<T>::operator + (Fraction<T> fraction) // + operator
 {
-	T numerator1 = lhs._numerator;
-	T denominator1 = lhs._denominator;
-	T numerator2 = rhs._numerator;
-	T denominator2 = rhs._denominator;
+	T numerator1 = this->_numerator;
+	T denominator1 = this->_denominator;
+	T numerator2 = fraction._numerator;
+	T denominator2 = fraction._denominator;
 	T denominatorR = denominator1 * denominator2;
 	T numeratorR = numerator1 * denominator2 + numerator2 * denominator1;
 	return Fraction<T>(numeratorR, denominatorR);
 }
 
 template <class T>
-Fraction<T> operator - (Fraction<T> & lhs, Fraction<T> & rhs) // - operator
+Fraction<T> Fraction<T>::operator - (Fraction<T> fraction) // - operator
 {
-	T numerator1 = lhs._numerator;
-	T denominator1 = lhs._denominator;
-	T numerator2 = rhs._numerator;
-	T denominator2 = rhs._denominator;
+	T numerator1 = this->_numerator;
+	T denominator1 = this->_denominator;
+	T numerator2 = fraction._numerator;
+	T denominator2 = fraction._denominator;
 	T denominatorR = denominator1 * denominator2;
 	T numeratorR = numerator1 * denominator2 - numerator2 * denominator1;
 	return Fraction<T>(numeratorR, denominatorR);
 }
 
 template <class T>
-Fraction<T> operator * (Fraction<T> & lhs, Fraction<T> & rhs) // * operator
+Fraction<T> Fraction<T>::operator * (Fraction<T> fraction) // * operator
 {
-	T numerator1 = lhs._numerator;
-	T denominator1 = lhs._denominator;
-	T numerator2 = rhs._numerator;
-	T denominator2 = rhs._denominator;
+	T numerator1 = this->_numerator;
+	T denominator1 = this->_denominator;
+	T numerator2 = fraction._numerator;
+	T denominator2 = fraction._denominator;
 	T numeratorR = numerator1 * numerator2;
 	T denominatorR = denominator1 * denominator2;
 	return Fraction<T>(numeratorR, denominatorR);
 }
 
 template <class T>
-Fraction<T> operator / (Fraction<T> & lhs, Fraction<T> & rhs) // / operator
+Fraction<T> Fraction<T>::operator / (Fraction<T> fraction) // / operator
 {
-	T numerator1 = lhs._numerator;
-	T denominator1 = lhs._denominator;
-	T numerator2 = rhs._numerator;
-	T denominator2 = rhs._denominator;
+	T numerator1 = this->_numerator;
+	T denominator1 = this->_denominator;
+	T numerator2 = fraction._numerator;
+	T denominator2 = fraction._denominator;
 	T numeratorR = denominator1 * denominator2;
 	T denominatorR = numerator1 * numerator2;
 	return Fraction<T>(numeratorR, denominatorR);
